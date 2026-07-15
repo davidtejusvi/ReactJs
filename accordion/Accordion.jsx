3284a2a0458c5431b2890a60ef432b844aa16515
David Tejusvi <davidtejusvi@gmail.com>
2026-07-16 00:05:59 +0530

    Add files via upload

---
+++ accordion/Accordion.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './accordion.css';
+
+const Accordion = ({ title, content }) => {
+    const [active, setActive] = useState(false);
+    return (
+        <section className='accordion-card' key={Math.random()}>
+            <div className='header' onClick={() => setActive(!active)}>
+                <div>{title}</div>
+                <p className='icon'>{active ? "-" : "+"}</p>
+            </div>
+            <div className='content'>
+                {
+                    active && <p className='card-info'>{content}</p>
+                }
+            </div>
+        </section>
+    )
+}
+
+export default Accordion
